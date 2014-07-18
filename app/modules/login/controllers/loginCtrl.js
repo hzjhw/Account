378e5b476ea14d6e9f67e8f8a2feb0d1b7165407
zhoujunjie88 <1113627163@qq.com>
2014-07-18 18:17:21 +0800

    login

--- app/modules/login/controllers/loginCtrl.js
+++ app/modules/login/controllers/loginCtrl.js
@@ -1,5 +1,5 @@
 app.controller('LoginCtrl', ['$scope', 'AccountFactory', function($scope, AccountFactory){
-    $scope.user = {};
+    $scope.user = {username: '',password: ''};
     $scope.submit = function(){
         AccountFactory.login($scope.user).then(function(data){
             alert('login success' + data.username);
