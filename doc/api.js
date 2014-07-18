b07d14c58c2a0a98b57f5d66095e84b419b15619
wyj <wyj@abbcc.net>
2014-07-18 18:06:54 +0800

    -

---
+++ doc/api.js
@@ -0,0 +1,10 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Est - 工具类库",
+        "factory - 工厂类"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
