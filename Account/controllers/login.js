7140af32038c5ca7f813fd956f8ea1bb630918cf
wyj <wyj@abbcc.net>
2014-08-13 08:32:36 +0800

    -

--- Account/controllers/login.js
+++ Account/controllers/login.js
@@ -8,30 +8,29 @@
  **/
 'use strict';
 
-app.config(["$routeProvider", function($routeProvider){
+app.config(["$routeProvider", function ($routeProvider) {
     $routeProvider.when('/login', {
-        templateUrl : 'modules/Account/views/login.html',
+        templateUrl: 'modules/Account/views/login.html',
         controller: "LoginCtrl",
         title: "用户登录"
     })
 }]);
 app.controller("LoginCtrl", ["$scope", "$rootScope", "$location", "$http", "API_END_POINT",
-    function($scope, $rootScope, $location, $http, API_END_POINT){
+    function ($scope, $rootScope, $location, $http, API_END_POINT) {
         // 局部变量
-        $scope.user = {username: '',password: ''};
-        if ($rootScope.isLogin) {
-            $location.path('/');
-        }
+        var site = $location.search()['site'];
+        var url = Est.isEmpty(site) ? '/' : site;
+        $scope.user = {username: '', password: ''};
+        if ($rootScope.isLogin) $location.path(url);
         // 表单提交
         $scope.submit = function () {
-            debugger
             $http.post(API_END_POINT + 'login', $scope.user).success(function (data) {
-                $location.path('/');
+                $location.path(url);
                 $rootScope.showMsg('登录成功！');
                 $location.reload();
             }).error(function (data) {
                 $rootScope.showMsg(data.err || '用户名或密码错误！', {
-                    type : 'danger'
+                    type: 'danger'
                 });
             });
         };
--- app/modules/login/controllers/loginCtrl.js
+++ app/modules/login/controllers/loginCtrl.js
@@ -1,31 +1,37 @@
-app.controller('LoginCtrl', ['$scope', 'AccountFactory','HOST','$window', function($scope, AccountFactory, HOST, $window){
-    $scope.user = {username: '',password: ''};
-    $scope.submit = function(){
-        AccountFactory.login($scope.user).then(function(data){
-            $window.location.href=HOST;
+app.controller('LoginCtrl', ['$scope', '$rootScope', '$location', 'AccountFactory', 'HOST', '$window','$q',
+    function ($scope, $rootScope, $location, AccountFactory, HOST, $window, $q) {
+
+        var site = $location.search()['site'];
+        var url = Est.isEmpty(site) ? HOST : site;
+        $scope.user = {username: '', password: ''};
+        AccountFactory.isLogin().then(function(){
+            $window.location.href = url;
         }, function(){
-            alert('登录失败');
         });
-    }
-}]);
-
-
+        $scope.submit = function () {
+            AccountFactory.login($scope.user).then(function (data) {
+                $window.location.href = url;
+            }, function () {
+                alert('登录失败');
+            });
+        }
+    }]);
 function r() {
-    var username=$("#username") ;
-    var pass=$("#password");
+    var username = $("#username");
+    var pass = $("#password");
     if (username.val() === "") {
         alert("请输入用户名!");
         username.focus();
         return;
     }
-    if (pass.val()=="") {
+    if (pass.val() == "") {
         alert("请输入密码");
         return;
     }
     return true;
 }
-$(function(){
-    $(".submit").bind('click', function(){
+$(function () {
+    $(".submit").bind('click', function () {
         r();
     })
 })
